55537fe4927b7829f20d74f2f252e37434543e77
Kolyada Danil <kd_kein@mail.ru>
2024-07-27 13:16:09 +1000

    7.15. Отрисуй меня полностью

--- js/data.js
+++ js/data.js
@@ -1,3 +1,5 @@
+import {getRandomInteger, getRandomUniqueNumberFromRange, getRandomElementFromArray} from './util.js';
+
 const COMMENT_MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,4 +20,50 @@ const NAMES = [
   'Вашингтон',
 ];
 
-export {COMMENT_MESSAGES, NAMES};
+/**
+ * Генерирует случайное количество комментариев
+ * @returns {Function} - замыкание создания списка комментариев
+ */
+function generateComments () {
+
+  const commentCount = getRandomInteger(1, 25); // Генерируем случайное количество комментариев
+  const commentsList = Array(); // Массив-список комментариев
+
+  for (let i = 0; i <= commentCount; i++) {
+    const commentID = getRandomUniqueNumberFromRange(1, 1000);
+
+    const randomAvatarNumber = getRandomInteger(1, 6);
+
+    const comment = {
+      id: commentID, // Любое число, не должны повторяться
+      avatar: `img/avatar-${randomAvatarNumber}.svg`, // Строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img
+      message: getRandomElementFromArray(COMMENT_MESSAGES), // Для формирования текста комментария необходимо взять одно или два случайных предложения из массива commentMessagesArray
+      name: getRandomElementFromArray(NAMES), // Должны быть случайными. Набор имён для комментаторов составьте сами
+    };
+
+    commentsList.push(comment);
+  }
+
+  return commentsList;
+}
+
+const photoID = getRandomUniqueNumberFromRange();
+const imageNameNumber = getRandomUniqueNumberFromRange();
+
+function getGeneratedPhoto () {
+
+  const comments = generateComments();
+  return {
+    id:   photoID(), // От 1 до 25, не должны повторяться
+    url: `photos/${imageNameNumber()}.jpg`, // Адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+    description: 'description', // Любая строка
+    likes: getRandomInteger(15, 200), // Случайное число от 15 до 200
+    comments: comments,
+  };
+}
+
+function generatePhotos (length) {
+  return Array.from({length: length}, getGeneratedPhoto);
+}
+
+export {generatePhotos};
